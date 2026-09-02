4246763c47a30ee1882ff24f8fdac498b130583c
G.Krishna <gundakrishna338@gmail.com>
2026-09-02 12:43:11 +0530

    adding problem_6

---
+++ Flask_practice/problem_6.py
@@ -0,0 +1,9 @@
+# HTML responses
+
+from flask import Flask
+app=Flask(__name__)
+@app.route('/')
+def home():
+    return "<h1>Welcome to Home<h1>"
+if __name__=='__main__':
+    app.run(debug=True)
